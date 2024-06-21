8c9340cc53d25537de59b06cc9eb81a4fb122b15
Isaac Boscà <isaac.bosca@exoticca.com>
2024-06-21 17:32:15 +0200

    Parse skeleton + regex

--- src/date-parser.ts
+++ src/date-parser.ts
@@ -1,10 +1,22 @@
 
 type DateString = String;
+type TimeOperator = '+' | '-' | '/';
+type TimeUnit = 'd' | 'M' | 'y' | 'h' | 'm' | 's' | 'w';
+
+interface TimeModifier {
+    timeOperator: TimeOperator,
+    timeUnit: TimeUnit,
+    timeAmount?: number | undefined
+}
 
 export class DateParser {
 
     public parse(datestring: DateString): Date {
-        return new Date();
+        const output: Date = new Date();
+        const timeModifiers: TimeModifier[] = this.buildTimeModifiersFromDateString(datestring);
+        return timeModifiers.reduce((carry, modifier): Date => {
+            return this.apply(modifier, carry);
+        }, output);
     }
     public stringify(date: Date): DateString {
         return '';
@@ -14,4 +26,18 @@ export class DateParser {
         return a + b;
     }
 
+    private buildTimeModifiersFromDateString(datestring: DateString): TimeModifier[] {
+        const operationalModifiers = datestring.match(/[+-]?\d+[dMyhmsw]/);
+        console.log(operationalModifiers);
+
+        const roundModifiers = datestring.match(/\/[dMyhmsw]/);
+        console.log(roundModifiers);
+
+
+        return [];
+    }
+
+    private apply(timeModifier: TimeModifier, date: Date): Date {
+        return date;
+    }
 }
