6d609c85acfaae0a59abc3bd91bf05b8a20c7c27
Isaac Boscà <isaac.bosca@exoticca.com>
2024-06-21 13:34:41 +0200

    Add required method signture

--- src/date-parser.ts
+++ src/date-parser.ts
@@ -1,5 +1,14 @@
 
+type DateString = String;
+
 export class DateParser {
+
+    public parse(datestring: DateString): Date {
+        return new Date();
+    }
+    public stringify(date: Date): DateString {
+        return '';
+    }
 
     public sum(a: number, b: number): number {
         return a + b;
