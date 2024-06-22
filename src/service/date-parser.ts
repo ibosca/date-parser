fe42ac3865f7dc0166753fe211490a617df04eee
Isaac Boscà <isaac.bosca@exoticca.com>
2024-06-22 17:24:41 +0200

    Stringify: time difference functions

--- src/service/date-parser.ts
+++ src/service/date-parser.ts
@@ -17,7 +17,57 @@ export class DateParser {
         }, new Date());
     }
     public stringify(date: Date): DateString {
-        return '';
+
+        // current: 2020-05-01T00:00:00.000Z
+        // date: 2019-05-01T00:00:00.000Z
+        // expected output: now-1y
+
+        const current = new Date();
+
+        const diff = new Date(date.getTime() - current.getTime());
+        console.log(`
+        Current: ${current.toISOString()} 
+        Date: ${date.toISOString()} 
+        
+        Years: ${this.yearDiff(current, date)}
+        Month: ${this.monthDiff(current, date)}
+        Week: ${this.weekDiff(current, date)}
+        Day: ${this.dayDiff(current, date)}
+        Hour: ${this.hourDiff(current, date)}
+        Minute: ${this.minuteDiff(current, date)}
+        Second: ${this.secondDiff(current, date)}
+        `);
+
+        return "";
+    }
+
+    private yearDiff(current: Date, date: Date): number {
+        return date.getUTCFullYear() - current.getUTCFullYear();
+    }
+
+    private monthDiff(current: Date, date: Date): number {
+        return date.getUTCMonth() - current.getUTCMonth();
+    }
+
+    private weekDiff(current: Date, date: Date): number {
+        return (date.getUTCDate() - current.getUTCDate()) / 7;
+    }
+
+    private dayDiff(current: Date, date: Date): number {
+        const days = (date.getUTCDate() - current.getUTCDate()) - this.weekDiff(current, date) * 7;
+        return days > 0 ? days : 0;
+    }
+
+    private hourDiff(current: Date, date: Date): number {
+        return date.getUTCHours() - current.getUTCHours();
+    }
+
+    private minuteDiff(current: Date, date: Date): number {
+        return date.getUTCMinutes() - current.getUTCMinutes();
+    }
+
+    private secondDiff(current: Date, date: Date): number {
+        return date.getUTCSeconds() - current.getUTCSeconds();
     }
 
 
--- tests/date-parser.test.ts
+++ tests/date-parser.test.ts
@@ -19,34 +19,48 @@ describe('Date Parser', () => {
         jest.useRealTimers();
     });
 
-    test('parse now-1y/y', () => {
-        const expected: Date = new Date('2019-01-01T00:00:00.000Z');
-        const actual: Date = sut.parse('now-1y/y');
-        expect(actual).toStrictEqual(expected);
-    });
+    describe('parse', () => {
+        test('now-1y/y', () => {
+            const expected: Date = new Date('2019-01-01T00:00:00.000Z');
+            const actual: Date = sut.parse('now-1y/y');
+            expect(actual).toStrictEqual(expected);
+        });
 
-    test('parse now/y', () => {
-        const expected: Date = new Date('2020-01-01T00:00:00.000Z');
-        const actual: Date = sut.parse('now/y');
-        expect(actual).toStrictEqual(expected);
-    });
+        test('now/y', () => {
+            const expected: Date = new Date('2020-01-01T00:00:00.000Z');
+            const actual: Date = sut.parse('now/y');
+            expect(actual).toStrictEqual(expected);
+        });
 
-    test('parse now-1d', () => {
-        const expected: Date = new Date('2020-04-30T00:00:00.000Z');
-        const actual: Date = sut.parse('now-1d');
-        expect(actual).toStrictEqual(expected);
-    });
+        test('now-1d', () => {
+            const expected: Date = new Date('2020-04-30T00:00:00.000Z');
+            const actual: Date = sut.parse('now-1d');
+            expect(actual).toStrictEqual(expected);
+        });
 
-    test('parse now+1d', () => {
-        const expected: Date = new Date('2020-05-02T00:00:00.000Z');
-        const actual: Date = sut.parse('now+1d');
-        expect(actual).toStrictEqual(expected);
-    });
+        test('now+1d', () => {
+            const expected: Date = new Date('2020-05-02T00:00:00.000Z');
+            const actual: Date = sut.parse('now+1d');
+            expect(actual).toStrictEqual(expected);
+        });
 
-    test('now-4d-4h', () => {
-        const expected: Date = new Date('2020-04-26T20:00:00.000Z');
-        const actual: Date = sut.parse('now-4d-4h');
-        expect(actual).toStrictEqual(expected);
-    });
+        test('now-4d-4h', () => {
+            const expected: Date = new Date('2020-04-26T20:00:00.000Z');
+            const actual: Date = sut.parse('now-4d-4h');
+            expect(actual).toStrictEqual(expected);
+        });
+    })
+
+    describe('stringify', () => {
+        test('now-1y', () => {
+            sut.stringify(new Date('2019-05-01T00:00:00.000Z'))
+        })
+
+        test('now+1y', () => {
+            sut.stringify(new Date('2021-05-01T00:00:00.000Z'))
+        })
+    })
 
 });
+
+
