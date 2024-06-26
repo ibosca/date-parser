6680ff4d93c320706d157b96e1c3c1d90cc4bc72
Isaac Boscà <isaac.bosca@exoticca.com>
2024-06-26 18:56:06 +0200

    Rename directory

--- src/service/date-parser.ts
+++ src/service/date-parser.ts
@@ -1,13 +1,13 @@
 import {DateChange} from "../domain/dateChange/date-change";
 import {DateChangeExtractor} from "../domain/dateChange/date-change-extractor";
-import {TimeDifferenceChecker} from "../domain/timeChecker/time-difference-checker";
-import {YearTimeDifferenceChecker} from "../domain/timeChecker/timeUnit/year-time-difference-checker";
-import {MonthTimeDifferenceChecker} from "../domain/timeChecker/timeUnit/month-time-difference-checker";
-import {WeekTimeDifferenceChecker} from "../domain/timeChecker/timeUnit/week-time-difference-checker";
-import {DayTimeDifferenceChecker} from "../domain/timeChecker/timeUnit/day-time-difference-checker";
-import {HourTimeDifferenceChecker} from "../domain/timeChecker/timeUnit/hour-time-difference-checker";
-import {MinuteTimeDifferenceChecker} from "../domain/timeChecker/timeUnit/minute-time-difference-checker";
-import {SecondTimeDifferenceChecker} from "../domain/timeChecker/timeUnit/second-time-difference-checker";
+import {TimeDifferenceChecker} from "../domain/timeDifferenceChecker/time-difference-checker";
+import {YearTimeDifferenceChecker} from "../domain/timeDifferenceChecker/timeUnit/year-time-difference-checker";
+import {MonthTimeDifferenceChecker} from "../domain/timeDifferenceChecker/timeUnit/month-time-difference-checker";
+import {WeekTimeDifferenceChecker} from "../domain/timeDifferenceChecker/timeUnit/week-time-difference-checker";
+import {DayTimeDifferenceChecker} from "../domain/timeDifferenceChecker/timeUnit/day-time-difference-checker";
+import {HourTimeDifferenceChecker} from "../domain/timeDifferenceChecker/timeUnit/hour-time-difference-checker";
+import {MinuteTimeDifferenceChecker} from "../domain/timeDifferenceChecker/timeUnit/minute-time-difference-checker";
+import {SecondTimeDifferenceChecker} from "../domain/timeDifferenceChecker/timeUnit/second-time-difference-checker";
 
 export type DateString = String;
 
